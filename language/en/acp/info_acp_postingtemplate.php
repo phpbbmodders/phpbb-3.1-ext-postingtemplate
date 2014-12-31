ea62eecdcbbaba5c69ab87d650c0264441d5550a
William Jacoby <bonelifer@gmail.com>
2014-12-31 07:22:01 -0600

    Fix newline problem and extra white space at the end after a comma

--- language/en/acp/info_acp_postingtemplate.php
+++ language/en/acp/info_acp_postingtemplate.php
@@ -20,5 +20,5 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'FORUM_POST_TPL'					=> 'Posting template',
-	'FORUM_POST_TPL_EXPLAIN'			=> 'The posting template is the text that appears in the textarea when starting a new topic in this forum. If you don’t wish to use it, just leave it blank.',	
+	'FORUM_POST_TPL_EXPLAIN'			=> 'The posting template is the text that appears in the textarea when starting a new topic in this forum. If you don’t wish to use it, just leave it blank.',
 ));
